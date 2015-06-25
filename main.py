3a84f366fae440d922b382c7b5709206012badcb
auermi <michael@michael-auer.co>
2015-06-25 16:02:50 -0500

    updated formatting

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ print("Hello, and welcome to the arena. In order to survive you must defeat your
 
 while True:
     # Player attack phase
-    print("Enter the respective command to take an action:\n[1] Attack\n[2] Quit")
+    print("Enter the respective command to take an action:\n    [ ] Attack\n    [ ] Quit")
     action = input("> ")
     if util.is_valid_input(action, "attack"):
         player.attack(enemy, 10)
