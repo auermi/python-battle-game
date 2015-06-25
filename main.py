7dbc9080a1b87a99787501a88f8501dc3fa93ceb
auermi <michael@michael-auer.co>
2015-06-25 15:43:41 -0500

    added enemy attack phase

--- main.py
+++ main.py
@@ -12,14 +12,19 @@ print("arena-battle.py")
 print("Hello, and welcome to the arena. In order to survive you must defeat your opponent.")
 
 while True:
+    # Player attack phase
     print("Enter the respective command to take an action:\n[1] Attack")
     action = input("> ")
-    if util.validateInput(action, "attack"):
+    if util.is_valid_input(action, "attack"):
         player.attack(enemy, 10)
-    
         print ("Your opponent has {} health left".format(enemy.health))
-
-    elif util.validateInput(action, "quit"):
+    elif util.is_valid_input(action, "quit"):
         break
     else:
         print ("Invalid input please try again.")
+
+    # Enemy Attack Phase
+    enemy.attack(player, 10)
+    print ("You have {} health left.".format(player.health))
+
+    
--- util.py
+++ util.py
@@ -1,6 +1,6 @@
 import re
 
 class Util:
-    def validateInput(test_case, inpt):
+    def is_valid_input(test_case, inpt):
         if re.search(test_case, inpt, flags=re.IGNORECASE):
             return True
